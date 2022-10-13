efcaf6defbb26f3b42b1dee1f331e156269e9193
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-10-13 08:50:46 -0500

    refactoring

--- js/script.js
+++ js/script.js
@@ -18,19 +18,15 @@
 // stampare numeri da 1 a 100
 
 for(let i = 1; i <= 100; i++) {
-  if(i % 3 === 0) {
+  if(!(i % 3)) {
     console.log('Fizz');
-  } else if (i % 5 === 0){
+  } else if (!(i % 5)){
     console.log('Buzz');
   } else {
-     console.log(i);
+    console.log(i);
   }
 }
 
-// per i multipli di 3 stampare Fizz al posto del numero
-
-
-// per i multipli di 5 stampare 
 
 
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
